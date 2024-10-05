b6330c3dde4ebbd5ea58d50333731c8d208b4c20
Ashutosh Prajapati <ashutosh1205vns@gmail.com>
2024-10-05 23:43:09 +0530

    setup bckend1

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default:axios}=require("axios");
 
-export const BASE_URL="https://linkedinclone-2z9g.onrender.com/"
+export const BASE_URL="https://linkedinclone-2z9g.onrender.com"
 export const clientServer=axios.create({
     baseURL:BASE_URL
 })
