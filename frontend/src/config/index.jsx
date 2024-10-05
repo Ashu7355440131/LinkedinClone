d88097557a9f796b31635c732612c90c774892ed
Ashutosh Prajapati <ashutosh1205vns@gmail.com>
2024-10-05 23:57:15 +0530

    frotend11

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default:axios}=require("axios");
 
-export const BASE_URL="https://linkedinclone-1-eziu.onrender.com/"
+export const BASE_URL="https://linkedinclone-1-eziu.onrender.com"
 export const clientServer=axios.create({
     baseURL:BASE_URL
 })
